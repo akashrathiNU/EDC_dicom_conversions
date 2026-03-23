4243934bfacd923b540fc97aa151c1276f84bd96
akashrathiNU <akash.rathi@northwestern.edu>
2026-03-23 13:50:14 -0500

    fixed qsm process

--- scripts/2_nifti_to_bids_naming.py
+++ scripts/2_nifti_to_bids_naming.py
@@ -196,6 +196,90 @@ def rename_partic(sub, ses, bids_dir):
         print(directory + new_name)
         os.rename(file, new_name)
 
+
+    # REST1 
+    files = sorted(glob.glob(directory + "*--PARTLYCLOUDY_2p4_ME5-*e1*"), key=str)
+    for file in files:
+        print(file)
+        parts = file.split(".", 1)
+        new_name = os.path.join(directory,"sub-" + sub + "_" + ses + "_task-rest_run-01_echo-1_bold." + parts[1])
+        print(directory + new_name)
+        os.rename(file, new_name) 
+
+    files = sorted(glob.glob(directory + "*--PARTLYCLOUDY_2p4_ME5-*e2*"), key=str)
+    for file in files:
+        print(file)
+        parts = file.split(".", 1)
+        new_name = os.path.join(directory,"sub-" + sub + "_" + ses + "_task-rest_run-01_echo-2_bold." + parts[1])
+        print(directory + new_name)
+        os.rename(file, new_name) 
+        
+    files = sorted(glob.glob(directory + "*--PARTLYCLOUDY_2p4_ME5-*e3*"), key=str)
+    for file in files:
+        print(file)
+        parts = file.split(".", 1)
+        new_name = os.path.join(directory,"sub-" + sub + "_" + ses + "_task-rest_run-01_echo-3_bold." + parts[1])
+        print(directory + new_name)
+        os.rename(file, new_name) 
+
+    files = sorted(glob.glob(directory + "*--PARTLYCLOUDY_2p4_ME5-*e4*"), key=str)
+    for file in files:
+        print(file)
+        parts = file.split(".", 1)
+        new_name = os.path.join(directory,"sub-" + sub + "_" + ses + "_task-rest_run-01_echo-4_bold." + parts[1])
+        print(directory + new_name)
+        os.rename(file, new_name) 
+
+    files = sorted(glob.glob(directory + "*--PARTLYCLOUDY_2p4_ME5-*e5*"), key=str)
+    for file in files:
+        print(file)
+        parts = file.split(".", 1)
+        new_name = os.path.join(directory,"sub-" + sub + "_" + ses + "_task-rest_run-01_echo-5_bold." + parts[1])
+        print(directory + new_name)
+        os.rename(file, new_name)
+
+
+# REST1 
+    files = sorted(glob.glob(directory + "*--PIPER_2p4_ME5-*e1*"), key=str)
+    for file in files:
+        print(file)
+        parts = file.split(".", 1)
+        new_name = os.path.join(directory,"sub-" + sub + "_" + ses + "_task-rest_run-02_echo-1_bold." + parts[1])
+        print(directory + new_name)
+        os.rename(file, new_name) 
+
+    files = sorted(glob.glob(directory + "*--PIPER_2p4_ME5-*e2*"), key=str)
+    for file in files:
+        print(file)
+        parts = file.split(".", 1)
+        new_name = os.path.join(directory,"sub-" + sub + "_" + ses + "_task-rest_run-02_echo-2_bold." + parts[1])
+        print(directory + new_name)
+        os.rename(file, new_name) 
+        
+    files = sorted(glob.glob(directory + "*--PIPER_2p4_ME5-*e3*"), key=str)
+    for file in files:
+        print(file)
+        parts = file.split(".", 1)
+        new_name = os.path.join(directory,"sub-" + sub + "_" + ses + "_task-rest_run-02_echo-3_bold." + parts[1])
+        print(directory + new_name)
+        os.rename(file, new_name) 
+
+    files = sorted(glob.glob(directory + "*--PIPER_2p4_ME5-*e4*"), key=str)
+    for file in files:
+        print(file)
+        parts = file.split(".", 1)
+        new_name = os.path.join(directory,"sub-" + sub + "_" + ses + "_task-rest_run-02_echo-4_bold." + parts[1])
+        print(directory + new_name)
+        os.rename(file, new_name) 
+
+    files = sorted(glob.glob(directory + "*--PIPER_2p4_ME5-*e5*"), key=str)
+    for file in files:
+        print(file)
+        parts = file.split(".", 1)
+        new_name = os.path.join(directory,"sub-" + sub + "_" + ses + "_task-rest_run-02_echo-5_bold." + parts[1])
+        print(directory + new_name)
+        os.rename(file, new_name)
+
     #T1w image
     counter = 0
     files = glob.glob(directory + "*MPRAGE*")
@@ -232,6 +316,146 @@ def rename_partic(sub, ses, bids_dir):
         new_name = os.path.join(directory,"sub-" + sub + "_" + ses + "_run-1_dir-PA_dwi." + parts[1])
         print(directory + new_name)
         os.rename(file, new_name)
+
+    #Tissue Iron Mapping
+    files = glob.glob(directory + "*QSM_8echo*e1.*")
+    for file in files:
+        print(file)
+        parts = file.split(".", 2)
+        new_name = os.path.join(directory,"sub-" + sub + "_" + ses + "_task-echo_flip-16_echo-1_part-mag_bold." + parts[2])
+        print(directory + new_name)
+        os.rename(file, new_name)
+
+    #Tissue Iron Mapping
+    files = glob.glob(directory + "*QSM_8echo*e2.*")
+    for file in files:
+        print(file)
+        parts = file.split(".", 2)
+        new_name = os.path.join(directory,"sub-" + sub + "_" + ses + "_task-echo_flip-16_echo-2_part-mag_bold." + parts[2])
+        print(directory + new_name)
+        os.rename(file, new_name)
+
+    #Tissue Iron Mapping
+    files = glob.glob(directory + "*QSM_8echo*e3.*")
+    for file in files:
+        print(file)
+        parts = file.split(".", 2)
+        new_name = os.path.join(directory,"sub-" + sub + "_" + ses + "_task-echo_flip-16_echo-3_part-mag_bold." + parts[2])
+        print(directory + new_name)
+        os.rename(file, new_name)
+
+    #Tissue Iron Mapping
+    files = glob.glob(directory + "*QSM_8echo*e4.*")
+    for file in files:
+        print(file)
+        parts = file.split(".", 2)
+        new_name = os.path.join(directory,"sub-" + sub + "_" + ses + "_task-echo_flip-16_echo-4_part-mag_bold." + parts[2])
+        print(directory + new_name)
+        os.rename(file, new_name)
+    #Tissue Iron Mapping
+    files = glob.glob(directory + "*QSM_8echo*e5.*")
+    for file in files:
+        print(file)
+        parts = file.split(".", 2)
+        new_name = os.path.join(directory,"sub-" + sub + "_" + ses + "_task-echo_flip-16_echo-5_part-mag_bold." + parts[2])
+        print(directory + new_name)
+        os.rename(file, new_name)
+    #Tissue Iron Mapping
+    files = glob.glob(directory + "*QSM_8echo*e6.*")
+    for file in files:
+        print(file)
+        parts = file.split(".", 2)
+        new_name = os.path.join(directory,"sub-" + sub + "_" + ses + "_task-echo_flip-16_echo-6_part-mag_bold." + parts[2])
+        print(directory + new_name)
+        os.rename(file, new_name)
+    #Tissue Iron Mapping
+    files = glob.glob(directory + "*QSM_8echo*e7.*")
+    for file in files:
+        print(file)
+        parts = file.split(".", 2)
+        new_name = os.path.join(directory,"sub-" + sub + "_" + ses + "_task-echo_flip-16_echo-7_part-mag_bold." + parts[2])
+        print(directory + new_name)
+        os.rename(file, new_name)
+    #Tissue Iron Mapping
+    files = glob.glob(directory + "*QSM_8echo*e8.*")
+    for file in files:
+        print(file)
+        parts = file.split(".", 2)
+        new_name = os.path.join(directory,"sub-" + sub + "_" + ses + "_task-echo_flip-16_echo-8_part-mag_bold." + parts[2])
+        print(directory + new_name)
+        os.rename(file, new_name)
+    #Tissue Iron Mapping
+    files = glob.glob(directory + "*QSM_8echo*e1_ph.*")
+    for file in files:
+        print(file)
+        parts = file.split(".", 2)
+        new_name = os.path.join(directory,"sub-" + sub + "_" + ses + "_task-echo_flip-16_echo-1_part-phase_bold." + parts[2])
+        print(directory + new_name)
+        os.rename(file, new_name)
+
+    #Tissue Iron Mapping
+    files = glob.glob(directory + "*QSM_8echo*e2_ph.*")
+    for file in files:
+        print(file)
+        parts = file.split(".", 2)
+        new_name = os.path.join(directory,"sub-" + sub + "_" + ses + "_task-echo_flip-16_echo-2_part-phase_bold." + parts[2])
+        print(directory + new_name)
+        os.rename(file, new_name)
+
+    #Tissue Iron Mapping
+    files = glob.glob(directory + "*QSM_8echo*e3_ph.*")
+    for file in files:
+        print(file)
+        parts = file.split(".", 2)
+        new_name = os.path.join(directory,"sub-" + sub + "_" + ses + "_task-echo_flip-16_echo-3_part-phase_bold." + parts[2])
+        print(directory + new_name)
+        os.rename(file, new_name)
+
+    #Tissue Iron Mapping
+    files = glob.glob(directory + "*QSM_8echo*e4_ph.*")
+    for file in files:
+        print(file)
+        parts = file.split(".", 2)
+        new_name = os.path.join(directory,"sub-" + sub + "_" + ses + "_task-echo_flip-16_echo-4_part-phase_bold." + parts[2])
+        print(directory + new_name)
+        os.rename(file, new_name)
+
+    #Tissue Iron Mapping
+    files = glob.glob(directory + "*QSM_8echo*e5_ph.*")
+    for file in files:
+        print(file)
+        parts = file.split(".", 2)
+        new_name = os.path.join(directory,"sub-" + sub + "_" + ses + "_task-echo_flip-16_echo-5_part-phase_bold." + parts[2])
+        print(directory + new_name)
+        os.rename(file, new_name)
+
+    #Tissue Iron Mapping
+    files = glob.glob(directory + "*QSM_8echo*e6_ph.*")
+    for file in files:
+        print(file)
+        parts = file.split(".", 2)
+        new_name = os.path.join(directory,"sub-" + sub + "_" + ses + "_task-echo_flip-16_echo-6_part-phase_bold." + parts[2])
+        print(directory + new_name)
+        os.rename(file, new_name)
+
+    #Tissue Iron Mapping
+    files = glob.glob(directory + "*QSM_8echo*e7_ph.*")
+    for file in files:
+        print(file)
+        parts = file.split(".", 2)
+        new_name = os.path.join(directory,"sub-" + sub + "_" + ses + "_task-echo_flip-16_echo-7_part-phase_bold." + parts[2])
+        print(directory + new_name)
+        os.rename(file, new_name)
+
+    #Tissue Iron Mapping
+    files = glob.glob(directory + "*QSM_8echo*e8_ph.*")
+    for file in files:
+        print(file)
+        parts = file.split(".", 2)
+        new_name = os.path.join(directory,"sub-" + sub + "_" + ses + "_task-echo_flip-16_echo-8_part-phase_bold." + parts[2])
+        print(directory + new_name)
+        os.rename(file, new_name)
+
 
 def main(sub, ses, bids_dir='/Users/akashrathi/Documents/Github/EDC_dicom_conversions/bids'):
     make_bids_dirs(sub,ses,bids_dir)
--- scripts/5_wholedir.sh
+++ scripts/5_wholedir.sh
@@ -3,11 +3,11 @@
 SES=$1
 echo "Session: $SES"
 
-for DIR in /Users/akashrathi/Documents/EDC_dicom_conversions/dicoms/uncompressed/*; do
+for DIR in /Users/akashrathi/Documents/Github/EDC_dicom_conversions/dicoms/uncompressed/*; do
     BASE_DIR=$(basename "$DIR")
 
-    if [ ! -d "/Users/akashrathi/Documents/EDC_dicom_conversions/bids/sub-$BASE_DIR/$SES/anat" ]; then
+    if [ ! -d "/Users/akashrathi/Documents/Github/EDC_dicom_conversions/bids/sub-$BASE_DIR/$SES/anat" ]; then
         echo "Processing subject: $BASE_DIR"
-        bash /Users/akashrathi/Documents/EDC_dicom_conversions/scripts/4_single_sub.sh "$BASE_DIR" "$SES"
+        bash /Users/akashrathi/Documents/Github/EDC_dicom_conversions/scripts/4_single_sub.sh "$BASE_DIR" "$SES"
     fi 
 done
