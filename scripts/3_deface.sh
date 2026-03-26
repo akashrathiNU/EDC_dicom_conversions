570b8ec1cdb8c8f75f1a273bd8cdd700872536b9
akashrathiNU <akash.rathi@northwestern.edu>
2026-03-26 09:19:01 -0500

    updated qsm

--- scripts/3_deface.sh
+++ scripts/3_deface.sh
@@ -4,9 +4,7 @@ SES=$2 #check if $ needed.
 echo $SES
 
 mv /Users/akashrathi/Documents/Github/EDC_dicom_conversions/bids/sub-$SUB/${SES}/*T1w.* /Users/akashrathi/Documents/Github/EDC_dicom_conversions/bids/sub-$SUB/${SES}/anat/
-#mv /Users/akashrathi/Documents/Github/EDC_dicom_conversions/bids/sub-$SUB/${SES}/*phase* /Users/akashrathi/Documents/Github/EDC_dicom_conversions/bids/sub-$SUB/${SES}/fmap/
-#mv /Users/akashrathi/Documents/Github/EDC_dicom_conversions/bids/sub-$SUB/${SES}/*mag* /Users/akashrathi/Documents/Github/EDC_dicom_conversions/bids/sub-$SUB/${SES}/fmap/
-#mv /Users/akashrathi/Documents/Github/EDC_dicom_conversions/bids/sub-$SUB/${SES}/*epi* /Users/akashrathi/Documents/Github/EDC_dicom_conversions/bids/sub-$SUB/${SES}/fmap/
+mv /Users/akashrathi/Documents/Github/EDC_dicom_conversions/bids/sub-$SUB/${SES}/*task-echo* /Users/akashrathi/Documents/Github/EDC_dicom_conversions/bids/sub-$SUB/${SES}/fmap/
 mv /Users/akashrathi/Documents/Github/EDC_dicom_conversions/bids/sub-$SUB/${SES}/*bold* /Users/akashrathi/Documents/Github/EDC_dicom_conversions/bids/sub-$SUB/${SES}/func/
 mv /Users/akashrathi/Documents/Github/EDC_dicom_conversions/bids/sub-$SUB/${SES}/*dwi* /Users/akashrathi/Documents/Github/EDC_dicom_conversions/bids/sub-$SUB/${SES}/dwi/
 pydeface --verbose /Users/akashrathi/Documents/Github/EDC_dicom_conversions/bids/sub-$SUB/${SES}/anat/sub-${SUB}_${SES}_run-1_T1w.nii.gz
